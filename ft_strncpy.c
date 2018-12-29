8bfaae4b24894c86acef9b08f4b24ca0a1ec58c9
marrodri <46203279+marrodri@users.noreply.github.com>
2018-12-28 18:01:24 -0800

    Update ft_strncpy.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -17,7 +17,7 @@ char	*ft_strncpy(char *dest, const char *src, size_t n)
 	size_t i;
 
 	i = 0;
-	while (src[i] != '\0' && (i < n) && dest[i] != '\0')
+	while (src[i] != '\0' && (i < n))// && dest[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
